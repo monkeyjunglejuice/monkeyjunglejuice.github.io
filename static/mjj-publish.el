f11b4e08bb4bfe81edce7ea3a42cbb00de2bb363
Dan Dee <monkeyjunglejuice@pm.me>
2025-01-04 14:24:05 +0800

    Update static files

--- static/mjj-publish.el
+++ static/mjj-publish.el
@@ -1,48 +1,77 @@
-;;; mjj-publish.el --- MJJ publishing settings  -*- lexical-binding: t; -*-
+;;; mjj-publish.el --- MJJ publishing machinery  -*- lexical-binding: t; -*-
 ;;; Commentary:
-;; Adds project to `org-publish-project-alist'
+;; The static website generator for MonkeyJungleJuice
 ;;; Code:
 
 ;;  ____________________________________________________________________________
-;;; USE-PACKAGE
-;; <https://github.com/jwiegley/use-package>
+;;; DEV WEBSERVER
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package nil))
+(defun mjj-launch-shell (command name)
+  "Launch programs via shell COMMAND. The NAME can be an arbitrary string.
+The sub-process can be managed via `list-processes'"
+  (start-process-shell-command
+   name
+   (concat "*Process " name "*")     ; with named buffer or `nil' without buffer
+   command)
+  (message (concat "Launching shell command " name "...done")))
 
-(eval-when-compile
-  (require 'use-package))
+;; TODO: Put the webserver project and binary into the blog directory
+(defun mjj-webserver-build ()
+  "(Re)build the webserver binary.
+The webserver is an Elixir application and requires a working Erlang/Elixir
+installation."
+  (interactive)
+  (message "TODO: Not implemented yet!"))
+
+(defun mjj-webserver-start ()
+  (interactive)
+  (mjj-launch-shell
+   "~/Code/webserver_ex/_build/dev/rel/webserver/bin/webserver start"
+   "MJJ Webserver"))
+
+(defun mjj-webserver-stop ()
+  (interactive)
+  (mjj-launch-shell
+   "~/Code/webserver_ex/_build/dev/rel/webserver/bin/webserver stop"
+   "MJJ Webserver"))
 
 ;;  ____________________________________________________________________________
 ;;; ORG EXPORT AND PUBLISHING
 
-;; <https://github.com/hniksic/emacs-htmlize>
-(use-package htmlize
-  :ensure t)
+(use-package org
+  :defer t
+  :config
+  ;; Additional structure templates for Org blocks
+  (add-to-list 'org-structure-template-alist '("n" . "nav"))
+  (add-to-list 'org-structure-template-alist '("m" . "message"))
+  (add-to-list 'org-structure-template-alist '("i" . "index"))
+  (add-to-list 'org-structure-template-alist '("t" . "tree"))
+  (add-to-list 'org-structure-template-alist '("b" . "box")))
+
+;; <https://orgmode.org/manual/Publishing.html>
+(use-package ox-publish)
 
 ;; <https://orgmode.org/manual/HTML-Export.html>
 (use-package ox-html
-  :ensure nil)
+  :defer t)
 
-;; <https://orgmode.org/manual/Publishing.html>
-(use-package ox-publish
-  :ensure nil)
+;; <https://github.com/hniksic/emacs-htmlize>
+(use-package htmlize
+  :defer t
+  :config
+  ;; Set external 'css' or 'inline-css' for code snippets created by htmlize
+  ;; CSS file can be generated with 'org-html-htmlize-generate-css'
+  (setq org-html-htmlize-output-type 'css))
+
+;; Prevent littering the recently visited files with exported HTML
+(use-package recentf
+  :defer t
+  :config
+  (add-to-list 'recentf-exclude
+               (expand-file-name "~/Documents/monkeyjunglejuice/.*\\.html$")))
 
 ;; Don't show day and hour
 ;; (setq org-html-metadata-timestamp-format "%Y-%m-%d")
-
-;;  ____________________________________________________________________________
-;;; PROJECT SPECIFIC
-
-;; Set external 'css' or 'inline-css' for code snippets created by htmlize
-;; CSS file can be generated with 'org-html-htmlize-generate-css'
-(setq org-html-htmlize-output-type 'css)
-
-;; Prevent littering the recently visited files with exported HTML
-(require 'recentf)
-(add-to-list 'recentf-exclude
-             (expand-file-name "~/Documents/monkeyjunglejuice/"))
 
 ;;  ____________________________________________________________________________
 ;;; PAGE TEMPLATE
@@ -50,14 +79,16 @@
 (defvar mjj-page-html-head nil "Lines to include in <head>...</head> specific to pages.")
 (setq mjj-page-html-head
       (concat
-       "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'self'; script-src 'self' https://wa.skred.net/; connect-src https://wa.skred.net/; img-src 'self' https://*; media-src 'self' https://*;\">\n"
+       "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'self'; script-src 'self' https://wa.skred.net/; connect-src https://wa.skred.net/; style-src 'self' https://*; img-src 'self' https://*; media-src 'self' https://*;\">\n"
        "<meta name=\"google-site-verification\" content=\"WUje0h1DXIUXINogHFZva1zk3Lw1oSpgrqva9dubYq0\">\n"
        "<link rel=\"stylesheet\" href=\"/static/normalize.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/org.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/style.css\" type=\"text/css\">\n"
+       "<link rel=\"stylesheet\" href=\"/static/fonts/iosevka-etoile/fonts.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/fonts/cormorant/fonts.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/fonts/gentium-basic/fonts.css\" type=\"text/css\">\n"
-       "<link rel=\"stylesheet\" href=\"/static/fonts/mononoki/fonts.css\" type=\"text/css\">\n"
+       "<link rel=\"stylesheet\" href=\"/static/fonts/iosevka-aile/fonts.css\" type=\"text/css\">\n"
+       "<link rel=\"stylesheet\" href=\"/static/fonts/iosevka/fonts.css\" type=\"text/css\">\n"
        "<link rel=\"icon\" type=\"image/svg+xml\" href=\"/static/monkeyjunglejuice.svg\">\n"
        "<link rel=\"apple-touch-icon\" href=\"/static/monkeyjunglejuice-icon180.png\">\n"
        "<script defer src=\"/static/header.js\"></script>\n"))
@@ -68,13 +99,14 @@
        "<nav id=\"nav-primary\">"
        "<a id=\"site-name\" href=\"/index.html\">MonkeyJungleJuice</a>"
        " "
+       "<a id=\"about\" href=\"/about.html\">About</a>"
        "<a id=\"github\" href=\"https://github.com/monkeyjunglejuice\">Github</a>"
        "</nav>"))
 
 (defvar mjj-page-postamble nil "Footer for pages.")
 (setq mjj-page-postamble
       (concat
-       "<div class=\"footer\">Published by <span class=\"author\">%a</span> <span class=\"email\">%e</span></div>\n"
+       "<div class=\"footer\">Published by <span class=\"author\"><a href=\"/about.html\">%a</a></span></div>\n"
        "<script defer src=\"/static/footer.js\"></script>"))
 
 ;;  ____________________________________________________________________________
@@ -83,13 +115,15 @@
 (defvar mjj-blog-html-head nil "Lines to include in <head>...</head> specific to blog articles.")
 (setq mjj-blog-html-head
       (concat
-       "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'self'; script-src 'self' https://wa.skred.net/; connect-src https://wa.skred.net/; img-src 'self' https://*; media-src 'self' https://*;\">\n"
+       "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'self'; script-src 'self' https://wa.skred.net/; connect-src https://wa.skred.net/; style-src 'self' https://*; img-src 'self' https://*; media-src 'self' https://*;\">\n"
        "<link rel=\"stylesheet\" href=\"/static/normalize.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/org.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/style.css\" type=\"text/css\">\n"
+       "<link rel=\"stylesheet\" href=\"/static/fonts/iosevka-etoile/fonts.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/fonts/cormorant/fonts.css\" type=\"text/css\">\n"
        "<link rel=\"stylesheet\" href=\"/static/fonts/gentium-basic/fonts.css\" type=\"text/css\">\n"
-       "<link rel=\"stylesheet\" href=\"/static/fonts/mononoki/fonts.css\" type=\"text/css\">\n"
+       "<link rel=\"stylesheet\" href=\"/static/fonts/iosevka-aile/fonts.css\" type=\"text/css\">\n"
+       "<link rel=\"stylesheet\" href=\"/static/fonts/iosevka/fonts.css\" type=\"text/css\">\n"
        "<link rel=\"icon\" type=\"image/svg+xml\" href=\"/static/monkeyjunglejuice.svg\">\n"
        "<link rel=\"apple-touch-icon\" href=\"/static/monkeyjunglejuice-icon180.png\">\n"
        "<script defer src=\"/static/header.js\"></script>"))
@@ -100,6 +134,7 @@
        "<nav id=\"nav-primary\">"
        "<a id=\"site-name\" href=\"/index.html\">MonkeyJungleJuice</a>"
        " "
+       "<a id=\"about\" href=\"/about.html\">About</a>"
        "<a id=\"github\" href=\"https://github.com/monkeyjunglejuice\">Github</a>"
        "</nav>\n"
        "<div class=\"info\"><time itemprop=\"dateModified\" datetime=\"%C\">Last updated: %C</time></div>"))
@@ -107,7 +142,7 @@
 (defvar mjj-blog-postamble nil "Footer for blog articles.")
 (setq mjj-blog-postamble
       (concat
-       "<div class=\"footer\">Published by <span class=\"author\">%a</span> <span class=\"email\">%e</span></div>\n"
+       "<div class=\"footer\">Published by <span class=\"author\"><a href=\"/about.html\">%a</a></span></div>\n"
        "<script defer src=\"/static/footer.js\"></script>"))
 
 ;;  ____________________________________________________________________________
@@ -140,8 +175,9 @@
                :auto-sitemap nil
                :preserve-breaks t
                :with-title t
+               :with-toc nil
                :with-smart-quotes t
-               :with-drawers t
+               :with-drawers nil
                :html-doctype "html5"
                :html-html5-fancy t
                :html-toplevel-hlevel 2
@@ -166,8 +202,9 @@
                :auto-sitemap nil
                :preserve-breaks t
                :with-title t
+               :with-toc nil
                :with-smart-quotes t
-               :with-drawers t
+               :with-drawers nil
                :html-doctype "html5"
                :html-html5-fancy t
                :html-toplevel-hlevel 2
@@ -180,4 +217,4 @@
 
 ;;  ____________________________________________________________________________
 (provide 'mjj-publish)
-;;; mjj0publish.el ends here
+;;; mjj-publish.el ends here
